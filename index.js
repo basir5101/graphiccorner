dd063ef48e82dfa3119aa12fba48c36ea6590fe9
Abdul Basir <basir.bsmrstu@gmail.com>
2021-07-04 18:02:40 +0600

    file upload

--- index.js
+++ index.js
@@ -25,3 +25,23 @@ const usableArea = (index) => {
         const value =  paragraph.innerHTML;
         console.log(value);
 }
+
+const uploadFile = () => {
+    const input =  document.getElementById('upload-file');
+    let file = input.files[0];
+    let fileSize = file.size/1000;
+    if(file.size > 25000000){
+        alert('file cannot be greater than 25 MB')
+    }
+        fileSize = fileSize + " KB";
+    document.getElementById('file-name').innerHTML = file.name;
+    document.getElementById('file-size').innerHTML = fileSize;
+    const closeElement = document.getElementById('close-file');
+          closeElement.style.cursor = 'pointer'
+          closeElement.innerHTML = 'X';
+    closeElement.addEventListener('click', () => {
+        closeElement.innerHTML = null;
+        document.getElementById('file-name').innerHTML = null;
+        document.getElementById('file-size').innerHTML = null;
+    })
+}
