fa01013c4c043bc57e8146286aa5eac5575c552c
Abdul Basir <basir.bsmrstu@gmail.com>
2021-07-04 17:19:45 +0600

    checked

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ const showHoursBudget = () =>{
     document.getElementById('hours-budget').style.display = 'block';
     document.getElementById('budget').style.display = "none"
 }
+
+const usableArea = (index) => {
+        const elements = document.querySelectorAll('.usableArea')
+        const element = elements[index];
+        element.style.background= 'rgb(231, 231, 231)';        
+        element.style.setProperty("--content", "'✔'");
+        const paragraph = element.lastElementChild;
+        const value =  paragraph.innerHTML;
+        console.log(value);
+}
